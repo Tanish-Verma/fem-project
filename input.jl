373ef642b833a3d35621e01bf3ba668f580345f0
Tanish Verma <verma.tanish@outlook.com>
2026-09-08 11:56:10 +0000

    Modified input to group 1's

--- input.jl
+++ input.jl
@@ -1,23 +1,23 @@
 function beamParameters()
-    beam_length = 8;          # m
-    E(x) = 1000^2 * 70;      # GPa -> KN/m^2
-    I(x) = 3.5*1e-4;        # m^4
+    beam_length = 4;          # m
+    E(x) = 20*10^3;      # GPa -> KN/m^2
+    I(x) = 1;        # m^4
     nElem = -1;
     nnpe = 2;
     return (; beam_length, E, I, nElem, nnpe)
 end
 
 function boundaryConditions()
-    nBC = 3;
-    bcLoc = [0, 0, 4];       # x-location of each support
-    bcDOF = [:w, :θ, :w];    # which DOF is restrained: :w (deflection) or :θ (rotation)
-    bcVal = [0, 0, 0];       # prescribed value at that DOF
+    nBC = 5;
+    bcLoc = [0, 0, 2, 4, 4];       # x-location of each support
+    bcDOF = [:w, :θ, :w, :w, :θ];    # which DOF is restrained: :w (deflection) or :θ (rotation)
+    bcVal = [0, 0, 0, 0, 0];       # prescribed value at that DOF
 
     return (; nBC, bcLoc, bcDOF, bcVal)
 end
 
 function forceAndMoments()
-    q(x) = if x < 4 0 else 9 end;                # kN/m
+    q(x) = if x < 3 0 else 24 end;                # kN/m
     nPointForce = 0;
     nPointMoment = 0;
     pfLoc = [];              # location(s) of point force(s)
@@ -28,9 +28,9 @@ function forceAndMoments()
 end
 
 function releases()
-    nReleases = 0;
-    relLoc  = [];       # x-location of release
-    relType = [];       # :v (shear release) or :m (moment release)
+    nReleases = 1;
+    relLoc  = [3];       # x-location of release
+    relType = [:m];       # :v (shear release) or :m (moment release)
 
     return (; nReleases, relLoc, relType)
 end
